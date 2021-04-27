82a6a12d70d5843289f811c2aef72bc84ad4fc0d
DESKTOP-FIRK1OT\DELL <nqkhanhlxag@gmail.com>
2021-04-27 10:59:00 +0700

    Init source

---
+++ src/main.js
@@ -0,0 +1,28 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(faUserSecret);
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import "normalize.css";
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue);
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin);
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  store,
+  render: (h) => h(App),
+}).$mount("#app");
