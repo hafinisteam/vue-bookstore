f75beeea2428fb4368c5db76b8d670a013a9000d
DESKTOP-FIRK1OT\DELL <nqkhanhlxag@gmail.com>
2021-05-03 11:44:04 +0700

    Add Register Sidebar

--- src/main.js
+++ src/main.js
@@ -2,25 +2,23 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-
-library.add(faUserSecret);
-
-Vue.component("font-awesome-icon", FontAwesomeIcon);
+import "@fortawesome/fontawesome-free/css/all.css";
+import "@fortawesome/fontawesome-free/js/all.js";
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 import "normalize.css";
 // Import Bootstrap an BootstrapVue CSS files (order is important)
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
-
+import "../src/assets/styles/main.scss";
 // Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue);
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin);
 Vue.config.productionTip = false;
-
+import PortalVue from "portal-vue";
+Vue.use(PortalVue);
+import Vuelidate from "vuelidate";
+Vue.use(Vuelidate);
 new Vue({
   router,
   store,
